62233379f42f625b1f2ed2820b6efd8f25123cbe
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-07-18 13:21:08 +0700

    [SecurityConfig] coding security part 2, catet 5

--- src/main/java/com/enigma/tokonyadia/security/SecurityConfiguration.java
+++ src/main/java/com/enigma/tokonyadia/security/SecurityConfiguration.java
@@ -1,25 +1,44 @@
 package com.enigma.tokonyadia.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    private final AuthTokenFIlter  authTokenFIlter;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.httpBasic().and().csrf().disable()
+         http.httpBasic().and().csrf().disable()
+                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                 .authorizeRequests()
+                 .antMatchers("/api/v1/auth/**").permitAll()
+                 .anyRequest().authenticated()
+                 .and().addFilterBefore(authTokenFIlter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+
+         return http.build();
     }
 }
